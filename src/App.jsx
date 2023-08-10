49f491c234cfff5b2e77c088038f9b5ede92e376
Karolis <karolisdarbinis2@gmail.com>
2023-08-10 12:59:00 +0300

    First commit

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import Header from "./components/Header";
+
+function App() {
+  return (
+    <div className="container">
+      <Header />
+    </div>
+  );
+}
+
+Header.defaultProps = {
+  title: "Task Tracker",
+};
+
+export default App;
---
+++ src/components/Header.jsx
@@ -0,0 +1,10 @@
+const Header = ({ title }) => {
+  return (
+    <header className="header">
+      <h1>{title}</h1>
+      <button className="btn">Add</button>
+    </header>
+  );
+};
+
+export default Header;
